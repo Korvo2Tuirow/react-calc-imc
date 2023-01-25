afea6ef0bbed1edc7f41fd6f52b1464359a2116f
Korvo2Tuirow <korvo2irow@live.com>
2023-01-25 20:37:22 -0300

    testes

--- Urna/Urna/script.js
+++ Urna/Urna/script.js
@@ -1,13 +1,13 @@
-let display1 = document.querySelector('#number1');
-let display2 = document.querySelector('#number2');
-let clear = document.querySelector('#clear');
+let display1 = document.querySelector("#number1");
+let display2 = document.querySelector("#number2");
+let clear = document.querySelector("#clear");
 let nome = document.querySelector("#name");
 let foto = document.querySelector("#image");
-let confirma = document.querySelector('#confirm');
-let urna = document.querySelector('#urna');
-let final = document.querySelector('#fim');
-let voltar = document.querySelector('#back');
-let container = document.querySelector('container');
+let confirma = document.querySelector("#confirm");
+let urna = document.querySelector("#urna");
+let final = document.querySelector("#fim");
+let voltar = document.querySelector("#back");
+let container = document.querySelector("container");
 
 alert("Numeros 13 ou 22 !!!");
 
@@ -15,15 +15,15 @@ function inserir(valor) {
   if (display1.innerHTML == "") {
     num1 = valor;
     display1.innerHTML = num1;
-   
   } else if (display2.innerHTML == "") {
     num2 = valor;
     display2.innerHTML = num2;
 
     concatenarNumeros();
+    mostrarDados();
   }
 }
-clear.addEventListener('click', limpar);
+clear.addEventListener("click", limpar);
 
 function limpar() {
   display1.innerHTML = "";
@@ -37,33 +37,30 @@ function concatenarNumeros() {
   concatena2 = display2.innerHTML;
   numeroCandidato = concatena1 + concatena2;
   numeroCandidato = parseInt(numeroCandidato);
-
-  dados = candidatos.filter(candidatoEscolhido => candidatoEscolhido.numero === numeroCandidato);
-
-  mostrarDados();
+  dados = candidatos.filter(
+    (candidatoEscolhido) => candidatoEscolhido.numero === numeroCandidato
+  );
 }
 
-function mostrarDados(){ 
+function mostrarDados() {
   nome.innerHTML = dados[0].nome;
   foto.innerHTML = dados[0].imagem;
 }
 
-confirma.addEventListener('click', confirmar);
+confirma.addEventListener("click", confirmar);
 
-function confirmar(){
- if(foto.innerHTML != ""){
-final.style.display = 'flex';
-final.classList.add('anima');
-container.style.display = 'none';
+function confirmar() {
+  if (foto.innerHTML != "") {
+    final.style.display = "flex";
+    final.classList.add("anima");
+    container.style.display = "none";
+  }
 }
-}
 
+voltar.addEventListener("click", back);
 
-voltar.addEventListener('click', back);
-
-function back(){
+function back() {
   //container.style.display = 'block';
-  final.style.display = 'none';
+  final.style.display = "none";
   limpar();
-};
-
+}
