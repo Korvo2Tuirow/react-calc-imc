520469b977590ed96f2ffa45b021a46cfaaeca2c
Korvo2Tuirow <korvo2irow@live.com>
2023-01-30 19:49:42 -0300

    poo aula

--- JAVASCRIPT/Objetos/script.js
+++ JAVASCRIPT/Objetos/script.js
@@ -1,18 +1,32 @@
-class Pessoa{
-    constructor(nome, idade, altura, imc){
+class Pessoa {
+  constructor(nome, idade, altura, peso, imc) {
+    this._nome = nome;
+    this._idade = idade;
+    this._altura = altura;
+    this._peso = peso;
+    this._imc = this._peso / (this._altura * this._altura);
+  }
 
-        this._nome = nome;
-        this._idade = idade;
-        this._altura = altura;
-        this._imc = imc;
-    }
+  calcularImc() {
+    return (this._peso / (this._altura * this._altura)).toFixed(2);
+  }
 
-    
+  //get = pegar informação
+  get nome() {
+    return this._nome;
+  }
 
+  set nome(novoNome) {
+    return (this._nome = novoNome);
+  }
 }
 
-let pessoa1 = new Pessoa("Robert", 38, 1.84);
-let pessoa2 = new Pessoa("Maria", 22, 1.50);
+let pessoa1 = new Pessoa("Robert", 38, 1.84, 84);
+let pessoa2 = new Pessoa("Maria", 22, 1.5, 55.3);
 
+pessoa1.nome = "Korvo";
+console.log(pessoa1._nome);
+
+console.log(pessoa1.calcularImc());
+console.log(pessoa2._idade);
 console.log(pessoa1);
-console.log(pessoa2);
