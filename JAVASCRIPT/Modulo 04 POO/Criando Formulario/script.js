a8d612feb2d32be5e5382c743a83a2c0e612731a
Korvo2Tuirow <korvo2irow@live.com>
2023-02-06 18:52:51 -0300

    Climatempo

---
+++ JAVASCRIPT/Modulo 04 POO/Criando Formulario/script.js
@@ -0,0 +1,20 @@
+class Input {
+    constructor(name, label){
+
+        _type = 'text';
+        required = false;
+
+        this.name= name;
+        this.label = label;
+
+    }
+
+    get type(){
+        return this._type;
+    }
+
+    set type(t){
+        if()
+    }
+
+}
--- Rodolfo Mori Live/script.js
+++ Rodolfo Mori Live/script.js
@@ -2,11 +2,27 @@ let btn = document.querySelector("button");
 let chave = "a6d29ceae26dbc378567cfced1f338fb";
 let dados;
 
+
 async function buscarCidade(cidade){
         dados = await fetch(
-        "https://api.openweathermap.org/data/2.5/weather?q="+cidade+'&appid='+chave)
-        .then(resposta => resposta.json());    
-    
+        "https://api.openweathermap.org/data/2.5/weather?q="+cidade+'&appid='+chave + "&lang=pt_br" + "&units=metric")
+        .then(resposta => resposta.json());  
+        
+       let city = dados.name;
+       document.querySelector("#cidade").innerHTML = ` ${city}`;
+
+       let temp = dados.main.temp;
+       document.querySelector("#temp").innerHTML = `Temperatura: ${temp.toFixed(0)}°C`;
+
+       let desc = dados.weather[0].description;
+       document.querySelector("#descricao").innerHTML = `Tempo: ${desc}`;
+
+       let umidade = dados.main.humidity;
+       document.querySelector("#umidade").innerHTML = `Umidade: ${dados.main.humidity}%` 
+       
+       let img = dados.weather[0].icon;
+       document.querySelector("#img").src ="https://openweathermap.org/img/wn/"+img+".png";
+
         console.log(dados);
     };
 
@@ -18,4 +34,4 @@ function buscar(){
     buscarCidade(cidade);
    
 
-   };
+};
