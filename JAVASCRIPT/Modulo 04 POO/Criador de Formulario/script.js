19c267ac394ca31fe94180530b6fdc7ad41b38ec
Korvo2irow <82429011+Korvo2Tuirow@users.noreply.github.com>
2023-02-06 10:32:05 -0300

    aulaPOO

---
+++ JAVASCRIPT/Modulo 04 POO/Criador de Formulario/script.js
@@ -0,0 +1,36 @@
+
+//AULA MUITO CONFUSA
+
+class Input{
+    _type = "text";
+    required = false;
+
+constructor(name, label){
+    this.name = name;
+    this.label = label;
+}
+
+get type(){
+    return this._type;
+}
+
+set type(t){
+    if(["text", "password", "email", "submit"].includes(t)){
+        this._type = t;
+    }else{
+        throw new Error(`Input "${t}" type doesn't exist`)
+    }
+}
+}
+
+//Email
+
+let email = new Input("email", "Digite seu email");
+email.type = "email";
+email.required = true;
+
+//senha
+
+let password = new Input("password", "Digite sua senha");
+password.type = "password";
+password.required = true;
