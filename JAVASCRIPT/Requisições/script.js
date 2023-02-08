edda8b3501d0c4b028dd6574d74e96393c724ad5
Korvo2Tuirow <korvo2irow@live.com>
2023-02-08 16:12:16 -0300

    then

--- JAVASCRIPT/Requisições/script.js
+++ JAVASCRIPT/Requisições/script.js
@@ -1,4 +1,6 @@
 let btn = document.querySelector("#btn");
+let url = "https://jsonplaceholder.typicode.com/todos";
+
 /*
 btn.addEventListener("click", ()=>{
   alert("clicou");
@@ -8,25 +10,32 @@ btn.addEventListener("click", ()=>{
 
 btn.addEventListener("click", clickCallback);
 
-function clickCallback(){
- fetch("https://jsonplaceholder.typicode.com/todos")
- .then(response => response.json())
- .then(json => console.log(json))
-
- 
-  
-};
-document.querySelector("#btn2").addEventListener('click', click);
-function click(){
-  fetch("https://dummyjson.com/products")
-  .then((resposta)=>{
-    return resposta.json(); // converte a resposta em um objeto
-   })
-
-   .then((dados) =>{
-    console.log(dados);
-      console.log(dados.products[10]);
-   })
-  
-
+function clickCallback() {
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => console.log(json));
 }
+
+document.querySelector("#btn2").addEventListener("click", click);
+function click() {
+  fetch("https://dummyjson.com/products")
+    .then((resposta) => {
+      return resposta.json(); // converte a resposta em um objeto
+    })
+
+    .then((dados) => {
+      console.log(dados);
+      console.log(dados.products[10]);
+    });
+}
+
+///////////////////////////////////////////////////////////
+//ORIGAMI https://www.youtube.com/watch?v=fhIDgAfuJZ8
+
+document.querySelector("#btn3").addEventListener("click", () => {
+  fetch("https://ranekapi.origamid.dev/json/api/produto")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+});
