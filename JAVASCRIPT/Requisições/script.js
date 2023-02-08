acc6d6f6dbc842a84c047741159f14e354a2a527
Korvo2Tuirow <korvo2irow@live.com>
2023-02-08 12:15:15 -0300

    del

--- JAVASCRIPT/Requisições/script.js
+++ JAVASCRIPT/Requisições/script.js
@@ -1,7 +1,32 @@
-function makeTable(){
+let btn = document.querySelector("#btn");
+/*
+btn.addEventListener("click", ()=>{
+  alert("clicou");
+}
+);
+*/
+
+btn.addEventListener("click", clickCallback);
+
+function clickCallback(){
+ fetch("https://jsonplaceholder.typicode.com/todos")
+ .then(response => response.json())
+ .then(json => console.log(json))
+
+ 
   
-    let tr = document.createElement(tr);
-    document.querySelector("#tbEstoque").appendChild (tr);
-    
-    
-  };
+};
+document.querySelector("#btn2").addEventListener('click', click);
+function click(){
+  fetch("https://dummyjson.com/products")
+  .then((resposta)=>{
+    return resposta.json(); // converte a resposta em um objeto
+   })
+
+   .then((dados) =>{
+    console.log(dados);
+      console.log(dados.products[10]);
+   })
+  
+
+}
