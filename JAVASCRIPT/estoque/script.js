a117ab2be5199d5a5d06d39bf90233cb2c8da2a2
Korvo2Tuirow <korvo2irow@live.com>
2023-02-08 11:30:33 -0300

    estoque

---
+++ JAVASCRIPT/estoque/script.js
@@ -0,0 +1,25 @@
+import { Produto } from "./poo.js";
+
+let descricaoAdd;
+let quantidadeAdd;
+let valorAdd;
+let p1;
+
+let enviar = document
+  .querySelector("#enviar")
+  .addEventListener("click", enviarDesc);
+
+function enviarDesc() {
+  event.preventDefault();
+  descricaoAdd = document.querySelector("#inputDesc").value;
+  quantidadeAdd = document.querySelector("#inputQuant").value;
+  valorAdd = document.querySelector("#inputValor").value;
+
+  p1 = new Produto(descricaoAdd, quantidadeAdd, valorAdd);
+
+  let d1 = (document.querySelector("#d1").innerHTML = p1._descricao);
+  let q1 = (document.querySelector("#q1").innerHTML = p1._quantidade);
+  let vu1 = (document.querySelector("#vu").innerHTML = p1._valor);
+  let vt = (document.querySelector("#vt").innerHTML = p1._valorTotal);
+  console.log(p1);
+};
