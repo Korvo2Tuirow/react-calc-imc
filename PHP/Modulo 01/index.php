0380fe7f2091bec1abbce9cfc87d9db4b2761d95
Korvo2Tuirow <korvo2irow@live.com>
2023-02-28 22:22:39 -0300

    php

---
+++ PHP/Modulo 01/index.php
@@ -0,0 +1,30 @@
+<?php
+$nome = 'Korvo';
+$sobrenome = 'Tuirow';
+$idade = 90;
+
+$nomeCompleto = "$nome $sobrenome tem $idade anos";
+
+echo $nomeCompleto;
+
+////////////////////////////////////
+
+$num1 = '3';
+$num2 = 4;
+$resultado = $num1+$num2;
+
+echo $resultado;
+
+//////////////////////////////////
+
+$string1 = 'ABC';
+$string2 = 'DEF';
+
+$strings = $string1;
+$strings .= $string2;// igual a $strings = $string1.$string2 
+
+echo $strings;
+
+//////////////////////////////////
+
+
