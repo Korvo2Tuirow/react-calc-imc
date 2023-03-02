ab8543ece6e012a7acc3a3455e41bcb223c680a9
Korvo2Tuirow <korvo2irow@live.com>
2023-03-02 15:10:15 -0300

    taDificil

--- PHP/Modulo 01/index.php
+++ PHP/Modulo 01/index.php
@@ -1,21 +1,23 @@
 <?php
+
+//VARIAVEIS////////////////////////
 $nome = 'Korvo';
 $sobrenome = 'Tuirow';
 $idade = 90;
 
 $nomeCompleto = "$nome $sobrenome tem $idade anos";
 
-echo $nomeCompleto;
+echo $nomeCompleto.'<br>';
 
-////////////////////////////////////
+////////
 
 $num1 = '3';
 $num2 = 4;
 $resultado = $num1+$num2;
 
-echo $resultado;
+echo $resultado.'<br>';
 
-//////////////////////////////////
+/////////
 
 $string1 = 'ABC';
 $string2 = 'DEF';
@@ -23,8 +25,29 @@ $string2 = 'DEF';
 $strings = $string1;
 $strings .= $string2;// igual a $strings = $string1.$string2 
 
-echo $strings;
+echo $strings.'<br>';
 
 //////////////////////////////////
 
+//ARRAYS/////////////////////////
+
+$ingredientes = ['açucar',
+                 'fermento',
+                 'ovo', 
+                 'leite', 
+                 'farinha'
+                ];
+
+echo $ingredientes[3].'<br>';
+
+$bolo = [
+    ...$ingredientes,
+    'corante',
+    'chocolate'   
+];
+
+echo '<h1>'.$bolo[6].'</h1>'.'<br>';
+print_r($bolo).'<br>';
+
+/////////////////////////////////////
 
