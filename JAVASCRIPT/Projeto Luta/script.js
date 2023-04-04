79ce05a1c461dd71eeca111d30b3cb91391c6d43
Korvo2Tuirow <korvo2irow@live.com>
2023-04-04 14:42:35 -0300

    poo ok

--- JAVASCRIPT/Projeto Luta/script.js
+++ JAVASCRIPT/Projeto Luta/script.js
@@ -1,4 +1,16 @@
-let char = new Knigth ('Korvo');
+let log = new Log(document.querySelector('.log'));
 
-console.log(char.life);
-console.log(char.name);
+
+let char = new Knigth("Korvo");
+let monster = new BigMonster();
+
+let stage = new Stage(
+  char,
+  monster,
+  document.querySelector("#char"),
+  document.querySelector("#monster"),
+  log
+
+);
+
+stage.start();
