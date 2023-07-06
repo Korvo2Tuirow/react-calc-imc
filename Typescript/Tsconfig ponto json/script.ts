ad9ac5cfea870799a1f3b0d4becea184bdba2953
Korvo2irow <82429011+Korvo2Tuirow@users.noreply.github.com>
2023-07-06 15:13:06 -0300

    ts

--- Typescript/Tsconfig ponto json/script.ts
+++ Typescript/Tsconfig ponto json/script.ts
@@ -5,3 +5,8 @@ const nome: string = "korvo";
 console.log(nome);
 
 let num:string = 80; // comando tsc mostra o erro mas gera o arquivo .js
+
+function somar(n1:number, n2:number) {
+return n1 + n2;
+}
+somar(10,20,30);
