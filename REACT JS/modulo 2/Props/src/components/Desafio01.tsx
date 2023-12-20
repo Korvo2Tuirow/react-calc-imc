934f1b5c522b3bcf2fcefbcff800bfb5193e341a
Korvo2irow <korvo2irow@live.com>
2023-12-20 18:55:10 -0300

    desafio01

--- REACT JS/modulo 2/Props/src/components/Desafio01.tsx
+++ REACT JS/modulo 2/Props/src/components/Desafio01.tsx
@@ -2,15 +2,17 @@ import {useState} from "react";
 import { photoList } from "@/photoList";
 
 export const Desafio01 = () =>{
-
+    const modal =()=>{
+        alert("OLA")
+    }
 
     return (
-        <div className="mx-auto">
-            <h1 className="text-3xl text-center">Imagens</h1>
+        <div className="mx-auto mb-20">
+            <h1 className="text-3xl text-center my-10">Imagens</h1>
             <section className="container max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                 {
                     photoList.map((item) =>(
-                        <div>...</div>
+                        <div className="hover:opacity-80 cursor-pointer" onClick={modal}><img src={`/assets/${item.url}`} alt=""  className="w-full" /></div>
                 ))}
             </section>
         
