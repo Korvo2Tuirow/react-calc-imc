796de12e52f06ba79c0b447227b2e9b8c9e0b268
Korvo2irow <korvo2irow@live.com>
2024-04-08 19:25:29 -0300

    Fazendo uma query

--- REACT JS/Modulo 6/tanstack/src/app/page.tsx
+++ REACT JS/Modulo 6/tanstack/src/app/page.tsx
@@ -1,10 +1,39 @@
 "use client"
 
-const Page = ()=>{
-  return(
-    <>
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+export type Post = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+}
+
+const Page = () => {
+
+  const query = useQuery({
+    queryKey: ['posts'],
+    queryFn: async (): Promise<Post[]> => {
+      const result = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      return result.data;
+    }
+  });
+
+  return (
+    <div>
       <h1 className="text-3xl text-white">Teste de TanStack</h1>
-    </>
+
+      {query.isLoading && "CARREGANDO"}
+
+      {query.data &&
+        <ul>
+          {query.data.map(item => (
+            <li key={item.id}>{item.title}</li>
+          ))}
+        </ul>
+      }
+    </div>
   )
 }
 
