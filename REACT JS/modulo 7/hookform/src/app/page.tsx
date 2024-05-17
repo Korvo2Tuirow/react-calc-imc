c13081d54fff1d7dc048b47c06cbe2869f9079b9
Korvo2irow <korvo2irow@live.com>
2024-05-17 15:41:40 -0300

    validação de erros

--- REACT JS/modulo 7/hookform/src/app/page.tsx
+++ REACT JS/modulo 7/hookform/src/app/page.tsx
@@ -12,7 +12,7 @@ type Inputs = {
 
 const Page = () => {
 
-  const { handleSubmit, register } = useForm<Inputs>();
+  const { handleSubmit, register, formState:{errors} } = useForm<Inputs>();
 
   const handleSubmitForm: SubmitHandler<Inputs> = (data) => {
     console.log(data);
@@ -28,27 +28,33 @@ const Page = () => {
         <h1 className="text-2xl">HOOK FORM</h1>
 
         <form onSubmit={handleSubmit(handleSubmitForm)}
-          className="flex flex-col gap-3 ">
+          className="flex flex-col gap-3 border p-5 rounded-md bg-white/10 ">
 
           <label htmlFor="name">Nome</label>
           <input {...register('name', { required: true, minLength:2, maxLength:20 })}
             placeholder="Nome"
             className="w-[500px] p-2 text-black" />
+            {errors.name?.type === "required" && <p className="text-red-400">"Campo obrigatório"</p>}
+            {errors.name?.type === "minLength" && <p className="text-red-400">"Nome no minimo com duas letras"</p>}
 
           <label htmlFor="lastName">Sobrenome</label>
           <input {...register('lastName', { required: true, minLength:2, maxLength:50 })}
             placeholder="Sobrenome"
             className="w-[500px] p-2 text-black" />
+             {errors.lastName?.type === "required" && <p className="text-red-400">"Campo obrigatório"</p>}
+            {errors.lastName?.type === "minLength" && <p className="text-red-400">"Nome no minimo com duas letras"</p>}
 
           <label htmlFor="idade">Idade</label>
-          <input {...register('idade', { required: true, min: 18, max:50 })}
+          <input {...register('idade', { required: '"Informe sua idade"', min: 18, max:50 })}
             type="number"
             placeholder="Idade"
-            className="w-[500px] p-2 text-black" />
+            className="w-[80px] p-2 text-black" />
+             {errors.idade && <p className="text-red-400">{errors.idade?.message}</p>}
+             {errors.idade?.type === "min" && <p className="text-red-400">"Idade minima 18 anos"</p>}
 
           <input type="submit"
             value="Enviar"
-            className="border border-gray-500 p-2 bg-green-700 font-bold cursor-pointer rounded-sm" />
+            className="border  border-gray-500 p-2 bg-green-700 font-bold cursor-pointer rounded-sm" />
 
         </form>
       </div>
