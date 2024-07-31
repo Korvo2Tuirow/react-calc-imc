516e5b799e395c3007a83a3c255cfce5a42bf30d
Korvo2irow <korvo2irow@live.com>
2024-07-30 22:04:37 -0300

    disable

--- REACT JS/NodeJs/IMC/react_calc_imc/src/App.tsx
+++ REACT JS/NodeJs/IMC/react_calc_imc/src/App.tsx
@@ -44,6 +44,7 @@ const App = () => {
             placeholder='Digite a sua altura. Ex: 1.5 (em metros)'
             value={heightField > 0 ? heightField : ''}
             onChange={e => setHeightField(parseFloat(e.target.value))}
+            disabled={toShow ? true : false}
           />
 
           <input
@@ -51,9 +52,10 @@ const App = () => {
             placeholder='Digite seu peso. Ex: 80.5 (em kg)'
             value={weightField > 0 ? weightField : ''}
             onChange={e => setWeightField(parseFloat(e.target.value))}
+            disabled={toShow ? true : false}
           />
 
-          <button onClick={handleCalculateButton}>Calcular</button>
+          <button onClick={handleCalculateButton} disabled={toShow ? true : false}>Calcular</button>
 
 
         </div>
