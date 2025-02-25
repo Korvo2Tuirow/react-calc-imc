58f208938a71be4dd3965e7a68310e0850b25ef0
Korvo2irow <korvo2irow@live.com>
2025-02-25 18:44:57 -0300

    teste de requisicao do prisma

--- REACT JS/NodeJs/first-next/src/app/api/getUrls/route.ts
+++ REACT JS/NodeJs/first-next/src/app/api/getUrls/route.ts
@@ -15,9 +15,30 @@ export async function GET() {
 
 GET().then(async(e) =>{
   await prisma.$disconnect();
-  console.log("Disconectar")
+  console.log("Disconectar" , e)
 }).catch(async(err) =>{
   console.log("Error: " + err);
   await prisma.$disconnect();
   process.exit(1)
 })
+
+
+
+//////////FORMA SIMPLIFICADA///////////////
+/*
+
+"use server"
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET() {  
+
+    const urls = await prisma.serverTv.findMany();
+
+    return urls.map(data => data);  
+}
+
+*/
+
--- REACT JS/NodeJs/first-next/src/components/form.tsx
+++ REACT JS/NodeJs/first-next/src/components/form.tsx
@@ -13,6 +13,7 @@ export const Form = () => {
   const { pending } = useFormStatus();
 
   return (
+
     <div className="flex flex-col justify-center items-center gap-3 shadow-custom bg-slate-700 border shadow-black p-5">
       
       <h1>SERVER ACTIONS</h1>
@@ -20,11 +21,15 @@ export const Form = () => {
       <form action={formAction} className="flex flex-col gap-5 w-3/4  items-center">
       
         <input type="text" name="name" placeholder="Digite seu nome" className="p-2 text-black rounded-md lg:w-3/4 sm:w-full  shadow-black shadow-md" />
+
         <input type="number" name="age" placeholder="Digite sau idade" className="p-2 text-black rounded-md w-30 shadow-black shadow-md" />
+
         <input type="submit" disabled={pending} value="Enviar" className="bg-green-700 w-1/4  min-w-16 p-2 rounded-md border shadow-black shadow-md hover:scale-105" />
+
         <p>{state.msg}</p>
 
       </form>
+      
     </div>
   )
 }
